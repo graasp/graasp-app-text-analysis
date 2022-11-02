fc0b3cb92d13d7fa51c4915afb709ff05cfff3c9
fannylsn <fanny.lasne@epfl.ch>
2022-11-02 19:16:34 +0100

    chore: added the key for the keyword (forgotten in the previous commit)

--- src/config/appSettingTypes.ts
+++ src/config/appSettingTypes.ts
@@ -1,3 +1,4 @@
 export const MOCK_SETTING_KEY = 'mock_setting';
 export const TEXT_RESOURCE_KEY = 'text_resource_setting';
 export const LESSON_TITLE_KEY = 'lesson_title_key';
+export const KEYWORDS_KEY = 'keywords_key';
