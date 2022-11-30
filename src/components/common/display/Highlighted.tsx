d277a2552c4da0e8f481327cd897bb679b19f384
fannylsn <91012518+fannylsn@users.noreply.github.com>
2022-11-30 17:56:48 +0100

    feat: add Markdown to text displayed in player view

--- src/components/common/display/Highlighted.tsx
+++ src/components/common/display/Highlighted.tsx
@@ -1,8 +1,10 @@
 import randomColor from 'randomcolor';
+import remarkBreaks from 'remark-breaks';
 
 import { FC } from 'react';
+import ReactMarkdown from 'react-markdown';
 
-import { Button } from '@mui/material';
+import { Button, styled } from '@mui/material';
 
 import { KEYWORD_BUTTON_CY } from '../../../config/selectors';
 
@@ -13,39 +15,75 @@ type Prop = {
   openChatbot: () => void;
 };
 
+const StyledReactMarkdown = styled(ReactMarkdown)(({ theme }) => ({
+  fontFamily: theme.typography.fontFamily,
+  '& *': {
+    marginBlockStart: 0,
+    marginBlockEnd: 0,
+  },
+  '& h1 p': {
+    marginTop: theme.spacing(1),
+  },
+  '& p': {
+    lineHeight: '1.5',
+    fontSize: '1rem',
+  },
+}));
+
 const Highlighted: FC<Prop> = ({ text, words, highlight, openChatbot }) => {
   if (!highlight || words.length === 0) {
-    return <span>{text}</span>;
+    return (
+      <StyledReactMarkdown remarkPlugins={[remarkBreaks]}>
+        {text}
+      </StyledReactMarkdown>
+    );
   }
 
   const expr = words.join('|');
   const parts = text.split(new RegExp(`(${expr})`, 'gi'));
   const wordsLowerCase = words.map((word) => word.toLocaleLowerCase());
+  const snippet = parts
+    .map((part) =>
+      wordsLowerCase.includes(part.toLocaleLowerCase()) ? `*${part}*` : part,
+    )
+    .join('');
+
+  // eslint-disable-next-line
+  const parseComponent = ({ children }: { children: any }) => {
+    const wordLowerCase = children[0].toLocaleLowerCase();
+    if (!wordsLowerCase.includes(wordLowerCase)) {
+      return <em>{wordLowerCase}</em>;
+    }
+    return (
+      <Button
+        data-cy={KEYWORD_BUTTON_CY}
+        sx={{
+          backgroundColor: randomColor({
+            seed: wordLowerCase,
+            luminosity: 'light',
+          }),
+          minWidth: '10px',
+          textTransform: 'none',
+          color: 'black',
+          fontWeight: 'inherit',
+          fontSize: 'inherit',
+          paddingY: '1px',
+        }}
+        onClick={openChatbot}
+      >
+        <span>{children[0]}</span>
+      </Button>
+    );
+  };
 
   return (
-    <span>
-      {parts.map((part, i) =>
-        wordsLowerCase.includes(part.toLocaleLowerCase()) ? (
-          <Button
-            data-cy={KEYWORD_BUTTON_CY}
-            sx={{
-              backgroundColor: randomColor({ seed: part.toLocaleLowerCase() }),
-              maxHeight: '23px',
-              minWidth: '10px',
-              textTransform: 'none',
-              color: 'black',
-              fontWeight: '400',
-            }}
-            key={i}
-            onClick={openChatbot}
-          >
-            {part}
-          </Button>
-        ) : (
-          <span key={i}>{part}</span>
-        ),
-      )}
-    </span>
+    <StyledReactMarkdown
+      components={{
+        em: parseComponent,
+      }}
+    >
+      {snippet}
+    </StyledReactMarkdown>
   );
 };
 
