2b345d13cfc3ec22ad1bd74d1fbeece3edcec1c2
fannylsn <fanny.lasne@epfl.ch>
2022-12-02 11:08:30 +0100

    empty URL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const ANONYMOUS_USER = 'Anonymous';
 export const ENTER_KEY = 'Enter';
-// export const CHATBOT_RESPONSE_URL =;
+export const CHATBOT_RESPONSE_URL = '';
 
 export const DICTIONARY_API_BASE_URL =
   'https://api.dictionaryapi.dev/api/v2/entries/en/';
