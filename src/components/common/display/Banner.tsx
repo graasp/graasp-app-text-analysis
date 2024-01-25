bd4799bbaaba16497ff4261112c50641260dc07f
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-01-25 16:29:32 +0100

    fix: use regex to find keywords - also use only one button to hide/display the keywords

--- src/components/common/display/Banner.tsx
+++ src/components/common/display/Banner.tsx
@@ -1,8 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { Box, Button, Typography } from '@mui/material';
 
-import { BANNER_CY, SUMMON_BUTTON_CY } from '../../../config/selectors';
+import { BANNER_CY, SHOW_KEYWORDS_BUTTON_CY } from '../../../config/selectors';
 import {
   DEFAULT_MARGIN,
   GRAASP_VIOLET,
@@ -10,57 +10,53 @@ import {
 
 type Prop = {
   title: string;
-  onSummonClick: () => void;
-  showDisable: boolean;
-  onHideClick: () => void;
-  hideDisable: boolean;
+  disabled: boolean;
+  onClick: (showKeywords: boolean) => void;
 };
 
-const Banner: FC<Prop> = ({
-  title,
-  onSummonClick,
-  showDisable,
-  onHideClick,
-  hideDisable,
-}) => (
-  <Box
-    data-cy={BANNER_CY}
-    component="span"
-    justifyContent="space-between"
-    display="flex"
-    alignItems="center"
-    sx={{ minHeight: '70px' }}
-  >
-    <Typography
-      variant="h4"
-      sx={{
-        color: GRAASP_VIOLET,
-        marginLeft: DEFAULT_MARGIN,
-      }}
+const Banner: FC<Prop> = ({ title, disabled, onClick }) => {
+  const [showKeywords, setShowKeywords] = useState(false);
+
+  // TODO: translate me
+  const SHOW_KEYWORDS_LABEL = 'show keywords';
+  const HIDE_KEYWORDS_LABEL = 'hide keywords';
+
+  const toggleKeywords = (): void => {
+    onClick(!showKeywords);
+    setShowKeywords(!showKeywords);
+  };
+
+  return (
+    <Box
+      data-cy={BANNER_CY}
+      component="span"
+      justifyContent="space-between"
+      display="flex"
+      alignItems="center"
+      sx={{ minHeight: '70px' }}
     >
-      {title}
-    </Typography>
-    <Box display="flex" flex-direction="row-reverse">
-      <Button
-        variant="contained"
-        sx={{ marginRight: DEFAULT_MARGIN }}
-        onClick={onHideClick}
-        disabled={hideDisable}
+      <Typography
+        variant="h4"
+        sx={{
+          color: GRAASP_VIOLET,
+          marginLeft: DEFAULT_MARGIN,
+        }}
       >
-        Hide Keywords
-      </Button>
-      <Button
-        data-cy={SUMMON_BUTTON_CY}
-        variant="contained"
-        color="success"
-        sx={{ marginRight: DEFAULT_MARGIN }}
-        onClick={onSummonClick}
-        disabled={showDisable}
-      >
-        Show Keywords
-      </Button>
+        {title}
+      </Typography>
+      <Box display="flex" flex-direction="row-reverse">
+        <Button
+          data-cy={SHOW_KEYWORDS_BUTTON_CY}
+          variant={showKeywords ? 'outlined' : 'contained'}
+          sx={{ marginRight: DEFAULT_MARGIN }}
+          onClick={toggleKeywords}
+          disabled={disabled}
+        >
+          {showKeywords ? HIDE_KEYWORDS_LABEL : SHOW_KEYWORDS_LABEL}
+        </Button>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default Banner;
