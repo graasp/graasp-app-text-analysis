d90c3bbcbb9f61327a8771ba882aa10dd56705e6
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-01-25 13:34:41 +0100

    chore: update graasp api host env

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,11 +3,10 @@
 interface ImportMetaEnv {
   readonly VITE_PORT: number;
   readonly VITE_VERSION: string;
-
   readonly VITE_GRAASP_APP_KEY: string;
   readonly VITE_GOOGLE_ANALYTICS_ID?: string;
   readonly VITE_ENABLE_MOCK_API: string;
-  readonly VITE_GRAASP_API_HOST?: string;
+  readonly VITE_API_HOST?: string;
 }
 
 interface ImportMeta {
